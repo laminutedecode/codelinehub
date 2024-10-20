d9e7bcea8a12903d4a09a1349ecfe74af2654064
Jbtzcodes <laminutedecode@gmail.com>
2024-10-20 19:29:39 +0200

    Update 20/20/2024

--- app/components/members/MemberContent.tsx
+++ app/components/members/MemberContent.tsx
@@ -12,20 +12,20 @@ export default function MemberContent({ userInfos }: UserProfileProps) {
 
         <div className="w-full  lg:w-[500px] lg:border-l px-4">
           <h3 className="text-lg font-bold text-white border-b border-purple-500 pb-2 mb-2">Langages et outils: </h3>
-          <ul className="flex items-center gap-4 flex-wrap mt-4">
-            {userInfos?.languages?.length ? (
-              userInfos.languages.map((language) => (
-                <li
-                  key={language}
-                  className="bg-purple-200 text-purple-500 p-2 rounded-md text-sm"
-                >
-                  {language}
-                </li>
-              ))
-            ) : (
-              <li className="text-gray-500">Non renseigné</li>
-            )}
-          </ul>
+            <ul className="flex items-center gap-4 flex-wrap mt-4">
+              {userInfos?.languages?.length ? (
+                userInfos.languages.map((language) => (
+                  <li
+                    key={language}
+                    className="bg-purple-200 text-purple-500 p-2 rounded-md text-[11px]"
+                  >
+                    {language}
+                  </li>
+                ))
+              ) : (
+                <li className="text-gray-500">Non renseigné</li>
+              )}
+            </ul>
         </div>
       </div>
   )
